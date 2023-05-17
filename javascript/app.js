5646b25b760411aa6f747aabf4463c06751ba10b
sawerarasheed <83883898+sawerarasheed@users.noreply.github.com>
2023-05-17 23:42:15 +0500

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -12,51 +12,7 @@ function removeNumb(){
 }
 
 function getRes(){
-    var inputExp = document.getElementById('result').value;
-  var ReplacedExp;
-  
-  switch(inputExp){
-   case 'π':
-     ReplacedExp=inputExp.replace('π','Math.PI');
-     
-  case 'sin':
-     ReplacedExp=inputExp.replace('sin','Math.sin');
-      
-  case 'sec':
-     ReplacedExp=inputExp.replace('sec','Math.sec');
-          
-  case 'cosec':
-     ReplacedExp=inputExp.replace('cosec','Math.cosec');
-     
-  case 'cos':
-     ReplacedExp=inputExp.replace('cos','Math.cos');
-     
-  case 'tan':
-     ReplacedExp=inputExp.replace('tan','Math.tan');
-   
-   
-  case 'log':
-     ReplacedExp=inputExp.replace('log','Math.log');
-   
-  case '√':
-     ReplacedExp=inputExp.replace('√','Math.radiac');
-   
-  case 'X':
-     ReplacedExp=inputExp.replace('X','Math.E');
-     
-  case 'e':
-     ReplacedExp=inputExp.replace('e','Math.e');
-     
-     break;
-     
-  default:
-  
-     break;
-   
-   
-   
-   
-  }
+    
   
     document.getElementById('result').value = eval(document.getElementById('result').value)
 }
